22107b55919572dd62ee898666bf5222ae575848
Thomas Jespersen <laumann.thomas@gmail.com>
2015-05-09 19:28:49 +0200

    Reorganise and add help message

--- src/bin/cargo-compiletest.rs
+++ src/bin/cargo-compiletest.rs
@@ -25,6 +25,7 @@ fn run_mode(mode: Mode, mut cfg: Config) {
     compiletest::run_tests(&cfg);
 }
 
+#[allow(non_upper_case_globals)]
 const all_modes: &'static [Mode] = &[
     Mode::CompileFail,
     Mode::ParseFail,
@@ -37,7 +38,79 @@ const all_modes: &'static [Mode] = &[
     Mode::Codegen,
 ];
 
+// Usage string
+const USAGE: &'static str = "Execute the compiletest subcommand
+
+Usage:
+    cargo compiletest [<args>...]";
+
+const HELPMSG: &'static str =
+"This utility is useful for testing negative cases, ie cases that are expected
+to not compile or run successfully.
+
+It requires a 'tests' folder and a separate folder for each mode.";
+
+fn usage(opts: &Options, exit_status: i32) -> ! {
+    print!("{}\n", opts.usage(USAGE));
+    print!("{}", HELPMSG);
+    std::process::exit(exit_status);
+}
+
+fn options() -> Options {
+    let mut opts = Options::new();
+    opts.optflag("v", "verbose", "Use verbose output")
+        .optflag("h", "help", "Print this help message and exit")
+        .optopt("m", "modes", "Specify a comma-separated list of modes  to run (default all)", "MODES");
+    opts
+}
+
+// Command-line arguments handling
+fn handle_args() -> Option<(Config, Vec<Mode>)> {
+    let mut config = compiletest::default_config();
+
+    let args: Vec<_> = env::args().skip(2).collect();
+    let opts = options();
+
+    let matches = match opts.parse(&args[..]) {
+        Ok(matches) => matches,
+        Err(e) => panic!("{}", e),
+    };
+
+    if matches.opt_present("h") {
+        usage(&opts, 0);
+    }
+
+    let modes: Vec<Mode> = match matches.opt_str("m") {
+        Some(s) => {
+            let mut err = false;
+            let modes = s.split(',').filter_map(|s| match s.parse() {
+                Ok(mode) => Some(mode),
+                Err(_)   => {
+                    println!("'{}' is not a valid mode", s);
+                    err = true;
+                    None
+                }
+            }).collect();
+            if err {
+                return None
+            }
+            modes
+        }
+        None => vec![]
+    };
+
+    config.verbose = matches.opt_present("v");
+
+    Some((config, modes))
+}
+
 fn main() {
+    let tests_folder = Path::new("tests");
+    if !tests_folder.is_dir() {
+        println!("error: No 'tests' folder were found");
+        usage(&options(), 1);
+    }
+
     let (config, mut modes) = match handle_args() {
         Some(opts) => opts,
         None => return
@@ -64,42 +137,4 @@ fn main() {
     if !any_tests_run {
         println!("No tests were found.");
     }
-}
-
-const USAGE: &'static str = "Execute the compiletest subcommand
-
-Usage:
-    cargo compiletest [<args>...]";
-
-fn usage(opts: &Options) -> ! {
-    print!("{}", opts.usage(USAGE));
-    std::process::exit(0);
-}
-
-fn handle_args() -> Option<(Config, Vec<Mode>)> {
-    let mut config = compiletest::default_config();
-
-    let args: Vec<_> = env::args().skip(2).collect();
-
-    let mut opts = Options::new();
-    opts.optflag("v", "verbose", "Use verbose output")
-        .optflag("h", "help", "Print this help message and exit")
-        .optopt("m", "modes", "Specify a comma-separated list of modes  to run (default all)", "MODES");
-
-    let matches = match opts.parse(&args[..]) {
-        Ok(matches) => matches,
-        Err(e) => panic!("{}", e),
-    };
-
-    if matches.opt_present("h") {
-        usage(&opts);
-    }
-
-
-
-    //cfg.mode = mode.parse().ok().expect("Invalid mode");
-
-    config.verbose = matches.opt_present("v");
-
-    Some((config, vec!()))
 }
